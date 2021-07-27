0d7b4b8871078bfd45182cc592d5f54094b3e2ef
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-07-27 22:21:53 +0200

    Create link to home.html
    Edit pages and  main urls using include to show pages/home.html
    Edit template dirs in main setttings.py
    Extends base. html in home.html

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse ()
+    return render(request,'pages/home.html')
 
 
