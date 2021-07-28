42af7571e1232d05111cc90acea88cff0edf30b4
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-07-28 21:00:17 +0200

    Edit pages titles , add navbar
    1. Add pages titles in functions in views and add {{title}} in templates
    2. Add simple navbar, for now in base.html not as a seperate navbar.html

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -4,14 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'pages/home.html')
-
+    return render(request,'pages/home.html', {'title':'Home'} )
 
 def about(request):
-    return render (request,'pages/about.html')
+    return render (request,'pages/about.html', {'title':'About'} )
 
 def news(request):
-    return render (request,'pages/news.html')
+    return render (request,'pages/news.html', {'title':'News'} )
 
 def contact(request):
-    return render (request,'pages/contact.html')
+    return render (request,'pages/contact.html', {'title':'Contact'} )
