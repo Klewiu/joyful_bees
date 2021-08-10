9e217bb35676a7bcb33093dd2173688630eaba25
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-08-10 22:16:10 +0200

    Add changes in Product model and template
    1. Change max_length in Product.description
    2. Add  func. to products.html temp - show colored stock availability

--- src/apps/products/views.py
+++ src/apps/products/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from apps.products.models import Product
+
 # Create your views here.
 
 def products(request):
     
     context = {
         'title':'Products',
-        'products': Product.objects.all()
+        'products': Product.objects.all(),
     }
     
     return render (request,'products/products.html', context)
