fd196d4c5492038f6a95c642985e7e6623d6e0c6
TomPrint <tomekklewicki@gmail.com>
2021-08-12 21:15:25 +0200

    initial changes to contact form

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import EmailField
 from django.shortcuts import render
 
 # from django.http import HttpResponse
@@ -14,5 +15,17 @@ def news(request):
     return render (request,'pages/news.html', {'title':'News'} )
 
 def contact(request):
-    return render (request,'pages/contact.html', {'title':'Contact'} )
+    if request.method =='POST':
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        context = {
+            'message_name': message_name,
+            'message_email': message_email,
+            'message': message,
+        }
+        return render (request,'pages/contact.html', context)
+
+    else:
+        return render (request,'pages/contact.html', {'title':'Contact'} )
 
