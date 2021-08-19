cf7d230031b4a67d0966c17e94e6bf3385f9c37c
TomPrint <tomekklewicki@gmail.com>
2021-08-20 00:16:46 +0200

    initial setup for creating a new post

--- src/apps/pages/urls.py
+++ src/apps/pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import NewsView, NewsDetailView
+from .views import NewsView, NewsDetailView, NewsCreateView
 
 urlpatterns = [
     path('', views.home, name='page-home'),
     path('about/', views.about, name='page-about'),
     path('news/', NewsView.as_view(), name='page-news'),
     path('news/<int:pk>/', NewsDetailView.as_view(), name='post-detail'),
+    path('news/new/', NewsCreateView.as_view(), name='news-create'),
     path('contact/', views.contact, name='page-contact'),
 ] 
