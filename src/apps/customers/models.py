c1a79d871c3d70718552923330548960713b74f6
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-09-01 23:13:32 +0200

    Manage cusomters with class base view
    1. Create class base views for customers
    2. Add absolute_url function in Cusomter model
    3.  Create manage_customers.html template
    4. Add new button from admin profile with link to manage customers

--- src/apps/customers/models.py
+++ src/apps/customers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.base import Model
 from phonenumber_field.modelfields import PhoneNumberField
+from django.urls import reverse
 
 # Create your models here.
 class Customer(models.Model):
@@ -12,6 +13,9 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('customers-view')
 
     def email_str():
         email_string = str(Customer.email)
--- src/apps/customers/views.py
+++ src/apps/customers/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from apps.customers.models import Customer
 from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
 
 def customers (request):
     context = {
-        'title':'Admin - Baza klientów',
+        'title':'Admin - Lista klientów',
         'customers': Customer.objects.all(),
         'recievers': Customer.email_all(),
     }
@@ -17,3 +19,70 @@ def customers (request):
     else:
         return redirect('page-home')
 
+# def manage_customers (request):
+#     context = {
+#         'title':'Admin - Zarządzanie klientami',
+#         'customers': Customer.objects.all(),
+#     }
+
+#     if request.user.is_authenticated and request.user.is_superuser:
+#         return render(request, 'customers/manage_customers.html', context)
+#     else:
+#         return redirect('page-home')
+    
+
+# CLASS BASE VIEW FOR CUSTOMERS #
+
+class CustomersView (ListView):
+
+  def get (self, request, *args, **kwargs):
+  
+    context = {
+        'customers': Customer.objects.all(),
+        'title':'ADMIN - lista klientów',
+      }
+    return render (request,'customers/manage_customers.html', context)
+
+class CustomersDetailView(DetailView):
+  model=Customer
+  template_name = 'customers/customers_detail.html'
+
+
+class CustomersCreateView (LoginRequiredMixin, CreateView):
+  model = Customer
+  fields = ['name', 'email', 'tel','address']
+  template_name = 'customers/customers_create.html'
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+class CustomersUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+  model=Customer
+  template_name = 'customers/customers_create.html'
+  fields = ['name', 'email', 'tel','address']
+
+  def form_valid(self, form):
+    # form.instance.author = self.request.user
+    return super().form_valid(form)
+
+#checks for currently logged  user to update posts related to current user
+#   def test_func(self):
+#     post = self.get_object()
+#     if self.request.user == post.author:
+#       return True
+#     else:
+#       return False
+
+class CustomersDeleteView(LoginRequiredMixin, UserPassesTestMixin,  DeleteView):
+  model=Customer
+  template_name = 'pages/customers_delete.html'
+
+#   def test_func(self):
+#     post = self.get_object()
+#     if self.request.user == post.author:
+#       return True
+#     else:
+#       return False
+
+  success_url = '/customers/'
