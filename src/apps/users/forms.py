b8c9fdeb853cb7bb2fc89a4aab7f687d6aa1331e
TomPrint <tomekklewicki@gmail.com>
2021-09-16 23:18:28 +0200

    Couple of small improvements

--- src/apps/users/forms.py
+++ src/apps/users/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext_lazy as _
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=40)
+    first_name = forms.CharField (max_length=40, label=_(u'Imię'))
     
     class Meta:
         model = User
