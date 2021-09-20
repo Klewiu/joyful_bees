16da5745297a7e57dc21f0adb97006364fbc02c6
TomPrint <tomekklewicki@gmail.com>
2021-09-20 22:34:05 +0200

    Update for Newsletter App

--- src/apps/newsletter/views.py
+++ src/apps/newsletter/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.db.models.fields import EmailField
+from django.shortcuts import render, redirect
 from .forms import NewsletterUserForm
+from django.contrib import messages
 
 # Create your views here.
 
 def newsletters(request):
-    
-    form =NewsletterUserForm()
+    if request.method =='POST':
+        form =NewsletterUserForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            email= form.cleaned_data.get('email')
+            messages.success(request, f'Dziękujemy za zapisanie adresu {email} do naszego newslettera!')
+            return redirect('/')
+            
+    else:
+        form =NewsletterUserForm()
     context = {
+        'title':'Newsletter',
         'form': form,
     }
     return render (request, 'newsletter/newsletter.html', context)
