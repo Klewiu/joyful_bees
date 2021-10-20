c90361462dcda269ced81c102d523c76f6a1f82f
TomPrint <tomekklewicki@gmail.com>
2021-10-20 23:15:07 +0200

    Few 'final touch' updates

--- src/joyful_bees/settings.py
+++ src/joyful_bees/settings.py
@@ -175,8 +175,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gosolomusic@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_KEY')
+EMAIL_HOST_USER = 'pasiekaradosc@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_KEY_1')
 
 
 MESSAGE_TAGS = {
